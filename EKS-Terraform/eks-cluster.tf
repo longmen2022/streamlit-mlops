3464b6040db47d5136c520dd2d0b83290128a9e1
men_l <men_long@yahoo.com>
2025-06-30 17:36:28 -0400

    update

--- EKS-Terraform/eks-cluster.tf
+++ EKS-Terraform/eks-cluster.tf
@@ -15,8 +15,6 @@ module "eks" {
     cluster = "demo"
   }
 
-  # Optional: Disable GPU/inference blocks that may be breaking your plan
-  manage_aws_auth_configmap = true
 
   eks_managed_node_group_defaults = {
     ami_type               = "AL2_x86_64"
